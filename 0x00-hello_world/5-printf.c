95e12031521186484ff5f33142afe99e26fed90d
mosab17 <mosabsnop@gimail.com>
2023-07-13 17:13:43 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * description: ac progrm printf function
+ *
+ * Return: alwsy 0 (secssus)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
