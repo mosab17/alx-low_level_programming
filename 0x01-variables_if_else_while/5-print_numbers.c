6919d321994d88d0d4e82dc2b3e14f7bda7184b6
mosab17 <mosabsnop@gimail.com>
2023-07-14 16:53:34 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entr point
+ *
+ * Description: print alphabte use putchar
+ *
+ * Return: 0 (seccuse)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
