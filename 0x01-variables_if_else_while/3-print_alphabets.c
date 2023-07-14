663bcf54a0ed713506b65cafc80973a9258abd04
mosab17 <mosabsnop@gimail.com>
2023-07-14 15:37:07 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entr point
+ *
+ * Description: print alphabte use putchar
+ *
+ * Return: 0 (seccuse)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (ch <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+		putchar('\n');
+	return (0);
+}
