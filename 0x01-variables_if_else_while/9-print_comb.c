5834b7cdfe15cfd4294d99021d3ee03f4fb9136d
mosab17 <mosabsnop@gimail.com>
2023-07-14 17:43:14 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,11 @@ int main(void)
 
 	while (digit <= 9)
 	{
-	putchar(digit + 48)
+	putchar(digit + 48);
 		if (digit != 9)
 		{
-			putchar(',')
-				putchar(' ')
+			putchar(',');
+				putchar(' ');
 		}
 		digit++;
 	}
