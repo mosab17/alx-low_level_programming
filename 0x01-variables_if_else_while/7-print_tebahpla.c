50015a32646a884a032fa012877255bcbd0cbe6d
mosab17 <mosabsnop@gimail.com>
2023-07-14 17:14:05 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entr point
+ *
+ * Description: print alphabte use putchar
+ *
+ * Return: 0 (seccuse)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= a)
+	{
+		putchar(ch);
+		digit--;
+	}
+	putchar('\n');
+	return (0);
+}
