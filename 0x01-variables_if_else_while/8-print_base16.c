52ec5000612510c5682ab02abeaaa6a4144f6985
mosab17 <mosabsnop@gimail.com>
2023-07-14 17:28:33 -0700

    c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entr point
+ *
+ * Description: print alphabte use putchar
+ *
+ * Return: 0 (seccuse)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(ch);
+		if (digit == 57)
+			digit += 39;
+				digit++;
+	}
+	putchar('\n');
+	return (0);
+}
