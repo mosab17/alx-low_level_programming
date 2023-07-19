651912f77f61d85fdfd3caf3b6cd43c949a1c8eb
mosab17 <mosabsnop@gimail.com>
2023-07-18 20:20:00 -0700

    v

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two ingegers
+ *
+ * @a: frist input
+ * @b: scond input
+ *
+ * Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
