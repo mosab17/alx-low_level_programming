04b2d715769ef5e437d0ce895f43bf76e02ca325
mosab17 <mosabsnop@gimail.com>
2023-07-19 15:28:47 -0700

    v

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int main(void)
 {
 	int count, initial0s;
 	unsigned long f1 = 1, f2 = 2, max = 10000000, f1o = 0, f2o = 0, sumo = 0;
-	unsigned long sum;
+	//unsigned long sum;
 
 	for (count = 1; count <= 98; count++)
 	{
@@ -49,7 +49,7 @@ int main(void)
 
 		printf("%lu", f1);
 
-		sum = (f1 + f2) % max;
+		//sum = (f1 + f2) % max;
 		sumo = f1o + f2o + (f1 + f2) / max;
 		f1 = f2;
 		f1o = f2o;
