0aecdce7641e4c623e1ff00c449540b380c8ae27
mosab17 <mosabsnop@gimail.com>
2023-07-19 10:46:26 -0700

    c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - enter point
+ *
+ * Description: print number of fabintcce
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
