ca8a7853bda9d3dc4916c9fa256911ea86c9a1cb
mosab17 <mosabsnop@gimail.com>
2023-07-18 17:49:08 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabte_x10 - function that will print alphabet 10 times
+ *
+*/
+
+void print_alphabte_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		-putchar('\n');
+	}
+}
