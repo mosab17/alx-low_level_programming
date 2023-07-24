da230094691cb5028a189f1c1a9eeb31a3efae9e
mosab17 <mosabsnop@gimail.com>
2023-07-24 16:34:27 -0700

    c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - test pointer
+ *
+ * @s: first input pointer
+ *
+ * Return: nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for  (l = 0; s[l] != '\0'; ++l)
+		;
+	for (I = 0; I < 1 / 2; i++)
+	{
+		Temp = s[i];
+		S[i] = s[l – 1 – i];
+		s[l – 1 – i] = temp;
+	}
+}
+
+
