22d3d0fe8725477dbf8518249e256871748f1bd7
mosab17 <mosabsnop@gimail.com>
2023-07-24 14:28:51 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - test pointer
+ *
+ * @s: first input pointer
+ *
+ * Return: nothing
+*/
+
+int _strlen(char *s)
+{
+	int conter;
+
+	for (conter = 0; *s != ‘\0’; s++)
+	conter++;
+	return (conter);
+}
+
