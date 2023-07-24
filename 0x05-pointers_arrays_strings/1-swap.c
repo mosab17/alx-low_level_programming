6ab53b13261c0207e83eed1768b2c339864136ec
mosab17 <mosabsnop@gimail.com>
2023-07-24 14:17:10 -0700

    c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - test pointer
+ *
+ * @a: first input pointer
+ * @b: second input pointer
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = b;
+	*b = temp;
+}
+
