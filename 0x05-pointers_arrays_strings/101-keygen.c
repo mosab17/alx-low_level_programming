6c0c5bca689d2b17f0ac0cff560f2cf090eaeb3e
mosab17 <mosabsnop@gimail.com>
2023-07-24 18:36:01 -0700

    c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+* main -  enter program cerit pass word progeam
+*
+* Description: password and new pass word
+*
+* Return: zero
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2654)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 – sum);
+	return (0);
+
+}
+
