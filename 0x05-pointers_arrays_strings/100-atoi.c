d82df6a76ad77c7cf4b0130ef6a001d54ab4fd0b
mosab17 <mosabsnop@gimail.com>
2023-07-24 18:14:06 -0700

    c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_atoi - test pointer
+ *
+ * @s: first input pointer
+ *
+ * Return: nothing
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sgin = 1;
+
+
+	do {
+		if (*s == '-')
+			sgin *= -1;
+	else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s – '0');
+	else if (num > 0)
+		break;
+	} while (*s++);
+	return (num * sign);
+}
+
+
