8fddb4f8d079683bddc7e0c2e7b00535a8b08d0f
mosab17 <mosabsnop@gimail.com>
2023-07-24 18:22:08 -0700

    c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (*s == '-')
 			sgin *= -1;
 	else if (*s >= '0' && *s <= '9')
-			num = num * 10 + *s – '0';
+			num = num * 10;
 	else if (num > 0)
 		break;
 	} while (*s++);
