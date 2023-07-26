68949aeb62426a727401fac01d0e52b9e79652e3
mosab17 <mosabsnop@gimail.com>
2023-07-26 12:00:45 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* *_strcat - function that concat
+*
+* @dest: frist char
+* @src: scond char
+*
+* Return: zero
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
+
+		return (dest);
+
+}
