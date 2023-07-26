d96a4dea1a39fa6133ffdf4baf6bbebc4600af2b
mosab17 <mosabsnop@gimail.com>
2023-07-26 15:09:52 -0700

    c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* reverse_array - function that concat
+*
+* @a: frist char
+* @n: scond char
+*
+* Return: zero
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+
+}
