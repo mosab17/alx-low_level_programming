bf77a18f1fc190912dbefe2a29d705f8f8d9e27a
mosab17 <mosabsnop@gimail.com>
2023-07-26 16:49:12 -0700

    c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* *cap_string - function that concat
+*
+*
+* @s: scond char
+*
+* Return: zero
+*/
+
+char *cap_string(char *s)
+{
+	int i;
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	ch *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == s2[i])
+			{
+				*s = s2[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+
+}
