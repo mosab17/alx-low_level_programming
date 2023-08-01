f85873ca97b7bec90e1383d01887f0545bffa076
mosab17 <mosabsnop@gimail.com>
2023-07-31 17:40:02 -0700

    c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+**_strstr - function file memory
+*
+* @haystack: frist input pointer
+* @needle: scond input
+*
+*
+*Return: pointer
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
