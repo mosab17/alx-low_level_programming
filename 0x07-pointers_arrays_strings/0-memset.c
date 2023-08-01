2076cad63dc1434b431c2c40ae1c0522e4e2993b
mosab17 <mosabsnop@gimail.com>
2023-07-31 17:01:05 -0700

    c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,9 +14,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsingned int itr;
 
-	for (itr = 0; n > 0; itr++, n--)
+	for (itr = 0; n > 0; itr++)
 	{
 		s[itr] = b;
+		n--;
 	}
 	return (s);
 }
