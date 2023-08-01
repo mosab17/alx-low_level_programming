ab976c8150006b4159345aaa8306f47fea344590
mosab17 <mosabsnop@gimail.com>
2023-07-31 18:11:01 -0700

    c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* set_string - file memory
+*
+* @s: frist input pointer
+* @to: sconde input
+*
+*
+*Return: pointer
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
