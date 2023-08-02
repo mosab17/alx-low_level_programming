b2959d4f4f4111bf1b8e0af79383c3fd44196ca3
mosab17 <mosabsnop@gimail.com>
2023-08-02 14:00:13 -0700

    c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - recursion
+*
+* @s: frist input pointer
+*
+*
+*
+*Return: pointer
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
+
+
+
+
+
