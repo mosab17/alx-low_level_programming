3bbf7aca1bf5085621d327ecbeaf0163d7499d20
mosab17 <mosabsnop@gimail.com>
2023-08-02 15:12:08 -0700

    c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+* _sqrt_recursion - recursion
+*
+* @n: frist input pointer
+*
+*
+*
+*Return: pointer
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+		return (actual_sqrt_recursion(n, 0));
+	}
+}
+
+/**
+* actual_sqrt_recursion - recursion
+*
+* @n: frist input pointer
+* @i: second input
+*
+*
+*Return: pointer
+*/
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
+
+
+
+
+
+
