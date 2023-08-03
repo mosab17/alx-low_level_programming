a1f45693cb76766dce241497c0d0afba21a13ad6
mosab17 <mosabsnop@gimail.com>
2023-08-03 06:02:59 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,10 +14,7 @@ int actual_sqrt_recursion(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-		return (actual_sqrt_recursion(n, 0));
-
+	return (actual_sqrt_recursion(n, 1));
 }
 
 /**
@@ -31,15 +28,10 @@ int _sqrt_recursion(int n)
 */
 int actual_sqrt_recursion(int n, int i)
 {
-	if (i * i > n)
-		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	else if (i * i < n)
+		return (actual_sqrt_recursion(n, i + 1));
+	else
+		return (-1);	
 }
-
-
-
-
-
-
