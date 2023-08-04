dd21c197f98f985a6f134ded4ccf31df5365c4e7
mosab17 <mosabsnop@gimail.com>
2023-08-04 16:32:04 -0700

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+* main - print program name
+*
+*@argc: intiger
+*@argv: const
+*
+*Return: 0
+*
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
