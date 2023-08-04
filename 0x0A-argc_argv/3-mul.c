a368d3577b5e34e5c0bdc32936c1165affe0a485
mosab17 <mosabsnop@gimail.com>
2023-08-04 16:54:10 -0700

    c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@
 *
 */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	int sum;
 
 	if (argc == 3)
 	{
-		sum = atoi(argv[1] * atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 	}
 	else
