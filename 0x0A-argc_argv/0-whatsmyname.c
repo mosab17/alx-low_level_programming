5cb5d80e91ca518a6e9a24019db9b0e2d0473ee7
mosab17 <mosabsnop@gimail.com>
2023-08-04 16:04:39 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+   #include <stdio.h>
+
+/**
+* main - print program name
+*
+*@argc: intiger
+*@argv: const
+*
+*Return: always 0 (seccues)
+*
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
