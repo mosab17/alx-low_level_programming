0d433fb5fbf59e8391b687304f93b6c68c5466a3
mosab17 <mosabsnop@gimail.com>
2023-08-04 16:22:24 -0700

    c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+* main - print program name
+*
+*@argc: intiger
+*@argv: const
+*
+*Return: 0
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+	int i, least = 0, mony = atoi(argv[1]);
+	int cent[] = {25, 10, 5, 2, 1};
+
+	for (i = 0; i < 5; i++)
+	{
+		if (mony >= cent[i])
+		{
+			least += mony / cent[i];
+			mony = mony % cent[i];
+			if (mony % cent[i] == 0)
+			{
+				break;
+			}
+
+		}
+
+	}
+	printf("%d\n", least);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
