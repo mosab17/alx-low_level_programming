13cae1670c3b1d9bbbf680cce86a41c8a7fa7221
mosab17 <mosabsnop@gimail.com>
2023-08-04 16:39:20 -0700

    c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+* main - print program name
+*
+*@argc: intiger
+*@argv: const
+*
+*Return: 0
+*
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++
+	}
+	return (0);
+}
