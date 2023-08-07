12ea77acb9f003a1a185739a26130dd9ab9bd9ca
mosab17 <mosabsnop@gimail.com>
2023-08-07 07:44:49 -0700

    c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* _strdup - recursion
+*
+* @str: frist input pointer
+*
+*
+*
+*Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, r = 0;
+	char *m;
+
+	while (str[i] != '\0')
+		i++;
+	m = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < str[r]; r++)
+			m[r] = str[r];
+	}
+	return (m);
+
+}
+
+
+
