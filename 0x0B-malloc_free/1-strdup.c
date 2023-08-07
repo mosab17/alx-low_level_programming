449f9344a9b3b635d2303b787964862ee876b4e6
mosab17 <mosabsnop@gimail.com>
2023-08-07 08:24:36 -0700

    c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,40 +1,29 @@
 #include "main.h"
 
 /**
-* _strdup - recursion
-*
-* @str: frist input pointer
-*
-*
-*
-*Return: 0
-*/
-
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
 char *_strdup(char *str)
 {
-	int i = 0, r = 0;
 	char *m;
+	int i, r = 0;
 
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-
+	i = 0;
 	while (str[i] != '\0')
 		i++;
+
 	m = malloc(sizeof(char) * (i + 1));
-	if (m == 0)
-	{
+
+	if (m == NULL)
 		return (NULL);
-	}
-	else
-	{
-		for (; i < str[r]; r++)
-			m[r] = str[r];
-	}
+
+	for (r = 0; str[r]; r++)
+		m[r] = str[r];
+
 	return (m);
-
 }
-
-
 
