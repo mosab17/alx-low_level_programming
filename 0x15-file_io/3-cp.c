ce4081578585f0a086b8b08f83c1565eaff2af92
mosab17 <mosabsnop@gmail.com>
2023-10-02 15:41:01 -0700

    c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -68,11 +68,11 @@ int main(int argc, char *argv[])
 
 	buffer = create_buffer(argv[2]);
 	fro = open(argv[1], O_RDONLY);
-	e = read(from, buffer, 1024);
+	e = read(fro, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (fro == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 		}
 
 		wr = write(to, buffer, e);
-		if (to == -1 || w == -1)
+		if (to == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
